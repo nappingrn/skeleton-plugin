aad2de3e6fe0a20641d7089992346c669b2d1c1c
nappingrn <dylan.r.trout@gmail.com>
2022-01-16 14:13:21 -0500

    inital commit

---
+++ src/main/java/net/runelite/client/plugins/friendtagging/DylansEpicPlugin.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2019. PKLite  - All Rights Reserved
+ * Unauthorized modification, distribution, or possession of this source file, via any medium is strictly prohibited.
+ * Proprietary and confidential. Refer to PKLite License file for more information on
+ * full terms of this copyright and to determine what constitutes authorized use.
+ * Written by PKLite(ST0NEWALL, others) <stonewall@thots.cc.usa>, 2019
+ *
+ */
+
+package net.runelite.client.plugins.friendtagging;
+
+import com.google.common.base.Strings;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.inject.Inject;
+import lombok.NonNull;
+import net.runelite.api.Client;
+import net.runelite.api.Friend;
+import net.runelite.api.Ignore;
+import net.runelite.api.MenuAction;
+import net.runelite.api.Nameable;
+import net.runelite.api.NameableContainer;
+import net.runelite.api.events.MenuEntryAdded;
+import net.runelite.api.events.MenuOptionClicked;
+import net.runelite.api.events.NameableNameChanged;
+import net.runelite.api.events.WidgetMenuOptionClicked;
+import net.runelite.api.util.Text;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.game.chatbox.ChatboxPanelManager;
+import net.runelite.client.game.chatbox.ChatboxTextInput;
+import net.runelite.client.menus.MenuManager;
+import net.runelite.client.menus.WidgetMenuOption;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import org.apache.commons.lang3.ArrayUtils;
+import org.pf4j.Extension;
+
+@Extension
+@PluginDescriptor(
+	name = "Dylan Tagging",
+	enabledByDefault = false,
+	description = "Tag people on your friends list.",
+	tags = {"PVP", "friend", "finder", "pk", "pklite"}
+)
+public class DylansEpicPlugin extends Plugin
+{
+
+}
